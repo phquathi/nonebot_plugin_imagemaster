a6837a5c4ee106f672277d8d78182a218b585612
phquathi <yangziqi233@gmail.com>
2024-06-11 15:45:55 +0800

    移除config

--- nonebot_plugin_imagemaster/__init__.py
+++ nonebot_plugin_imagemaster/__init__.py
@@ -1,10 +1,7 @@
 from nonebot import get_driver
 from nonebot.plugin import PluginMetadata
 
-from .config import Config
-
-global_config = get_driver().config
-config = Config.parse_obj(global_config)
+from .bot_main import *
 
 __plugin_meta__ = PluginMetadata(
     name="nonebot-plugin-imagemaster",
